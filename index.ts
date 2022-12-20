748cc3d7f93443a7eb1017a938f7ea519bd64f93
TurboTechLab <42316316+TurboTechLab@users.noreply.github.com>
2022-12-20 16:27:02 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -7,14 +7,14 @@ const coldByDefault = new Observable((observer) => {
   observer.complete();
 });
 
-const subject = new Subject();
-const hotWithConnectable = connectable(coldByDefault, {
-  connector: () => new Subject()
-});
-//Now all the subscriptions share the same data stream for each connect
-hotWithConnectable.subscribe((v) => console.log(v));
-hotWithConnectable.subscribe((v) => console.log(v));
-hotWithConnectable.subscribe((v) => console.log(v));
+const hotWithConnectable = connectable(
+    coldByDefault, 
+    { connector: () => new Subject()}
+);
 
-
+//Now all the subscriptions will share the same data stream 
+hotWithConnectable.subscribe((v) => console.log(v)),
+hotWithConnectable.subscribe((v) => console.log(v)), 
+hotWithConnectable.subscribe((v) => console.log(v));
+//connect will start streaming the data
 hotWithConnectable.connect();
